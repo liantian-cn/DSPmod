3254e1744890c70a42621410f083571d73247817
liantian <liantian.me+code@gmail.com>
2021-10-02 18:17:17 +0800

    1
    1

--- DisableGameAbnormalityCheck/DisableGameAbnormalityCheck.cs
+++ DisableGameAbnormalityCheck/DisableGameAbnormalityCheck.cs
@@ -4,31 +4,29 @@ using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 using BepInEx.Configuration;
+using System.Reflection;
 
 
 namespace DisableGameAbnormalityCheck
 {
-    [BepInPlugin("me.liantian.plugin.PuttyVein", "PuttyVein", "1.0.1")]
+    [BepInPlugin("me.liantian.plugin.DisableGameAbnormalityCheck", "DisableGameAbnormalityCheck", "1.0.1")]
     [BepInProcess("DSPGAME.exe")]
     public class DisableGameAbnormalityCheck : BaseUnityPlugin
     {
 
 
 
-		public new static ManualLogSource Logger;
 		public static RectTransform pppButton = null;
 
 		public void Awake()
 		{
-
-			DisableGameAbnormalityCheck.Logger = base.Logger;
-			Harmony.CreateAndPatchAll(typeof(DisableGameAbnormalityCheck));
-			DisableGameAbnormalityCheck.Logger.LogInfo("DisableGameAbnormalityCheck 初始化");
+			Logger.LogInfo("DisableGameAbnormalityCheckis 初始化");
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 		}
 
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(GameAbnormalityCheck), "isGameNormal")]
-		public static bool isGameNormalPatch(ref bool __result)
+		public static bool CheckGameNormalPatch(ref bool __result)
 		{
 			__result = true;
 			return false;
@@ -36,26 +34,14 @@ namespace DisableGameAbnormalityCheck
 
 
 		[HarmonyPrefix]
-		[HarmonyPatch(typeof(GameAbnormalityCheck), "IsFunctionNormal")]
-		public static bool IsFunctionNormalPatch(ref bool __result)
+		[HarmonyPatch(typeof(GameAbnormalityCheck), nameof(GameAbnormalityCheck.AfterTick))]
+		[HarmonyPatch(typeof(GameAbnormalityCheck), nameof(GameAbnormalityCheck.BeforeTick))]
+		[HarmonyPatch(typeof(GameAbnormalityCheck), nameof(GameAbnormalityCheck.CheckMajorClause))]
+		[HarmonyPatch(typeof(GameAbnormalityCheck), nameof(GameAbnormalityCheck.NotifyAbnormalityChecked))]
+		[HarmonyPatch(typeof(MilkyWayWebClient), nameof(MilkyWayWebClient.SendReportRequest))]
+		[HarmonyPatch(typeof(MilkyWayWebClient), nameof(MilkyWayWebClient.SendUploadRecordRequest))]
+		public static bool Prefix()
 		{
-			__result = true;
-			return false;
-		}
-
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MilkyWayWebClient), "SendUploadRecordRequest")]
-		public static bool SendUploadRecordRequestPatch(GameData gameData)
-		{
-
-			return false;
-		}
-
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MilkyWayWebClient), "SendReportRequest")]
-		public static bool SendReportRequestPatch()
-		{
-
 			return false;
 		}
 
